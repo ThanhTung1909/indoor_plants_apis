8554963d0d2c04fd9adc04487d4613e1f86a70ad
ThanhTung1909 <phamthanhtung200698@gmail.com>
2025-04-22 08:00:34 +0700

    create uploadCloud

--- src/api/v1/routes/index.route.ts
+++ src/api/v1/routes/index.route.ts
@@ -6,7 +6,9 @@ import {cartRouter} from "./cart.router"
 const mainV1Routes = (app: Express) => {
     const version: String = "/api/v1";
 
-    app.use(version + "/plants", plantRoutes);
+    app.use(version + "/plants", plantRoutes)
+    app.use(version + "/user", userRoutes)
+    app.use(version + "/cart", cartRouter)
 }
 
 export default mainV1Routes;
