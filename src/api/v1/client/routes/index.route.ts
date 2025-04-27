3e06a50980bc4a09c858feef5f95b1aa2ee0538d
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2025-04-27 19:55:09 +0700

    save

---
+++ src/api/v1/client/routes/index.route.ts
@@ -0,0 +1,21 @@
+import { Express } from "express";
+import { plantRoutes } from "../../plant.route";
+import { userRoutes } from "./user.route";
+import {cartRouter} from "./cart.router"
+import { orderRouter } from "./order.router";
+
+import * as authMiddleware from "../../../../middlewares/auth.middleware"
+
+const mainV1Routes = (app: Express) => {
+    const version: String = "/api/v1";
+
+    app.use(version + "/plants", plantRoutes);
+    app.use(version + "/user", userRoutes);
+    app.use(version + "/cart", cartRouter);
+    app.use(version + "/order", orderRouter);
+    // app.use(version + "/plants", plantRoutes)
+    // app.use(version + "/user", userRoutes)
+    // app.use(version + "/cart", authMiddleware.requireAuth, cartRouter)
+}
+
+export default mainV1Routes;
