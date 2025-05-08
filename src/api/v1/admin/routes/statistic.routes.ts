b739def7cb6b884a5877f4c0ad8f8e538627a966
ThanhTung1909 <phamthanhtung200698@gmail.com>
2025-05-08 23:28:24 +0700

    update statistic

--- src/api/v1/admin/routes/statistic.routes.ts
+++ src/api/v1/admin/routes/statistic.routes.ts
@@ -3,5 +3,8 @@ import * as statisticControllers from "../controllers/statistic.controllers";
 const router: Router = Router();
 
 router.get("/summary", statisticControllers.getPlantOrderSummary);
+router.get("/order-status", statisticControllers.getOrderStatus)
+router.get("/activities", statisticControllers.getRecentUserActivities)
+router.get("/overview", statisticControllers.getOverview)
 
 export const statisticRoutes: Router = router;
